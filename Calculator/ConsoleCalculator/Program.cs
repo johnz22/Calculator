014093552321d75b14a093073e0eba7189ad565f
john_z22@hotmail.com <@1#2f4T%>
2022-03-21 22:04:11 +1100

    My First Calculator

---
+++ Calculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+public class Calculator
+{
+    public static void Main()
+    {
+        int Number = 0;
+
+        while (Number == 0)
+        {
+            Console.Write("Enter number: ");
+            try
+            {
+                Number = Convert.ToInt32(Console.ReadLine());
+                if (Number < 1 || Number > 10)
+                {
+                    Console.WriteLine("Please enter number between 1 to 10");
+                    Number = 0;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter numeric value number between 1 to 10");
+            }
+        }
+
+        for (int i = 0; i <= 10; i++)
+        {
+            if (i == 11)
+                break;
+            Console.WriteLine("{0} x {1} = {2}", Number, i , i * Number);
+        }
+    }
+}
