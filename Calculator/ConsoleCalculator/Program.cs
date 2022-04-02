d0f8329db1fe44db1f0bec8224d4d0cdb464ecbf
DotNetCore <36761115+johnz22@users.noreply.github.com>
2022-04-02 23:53:49 +1100

    My First Calculator

--- Calculator/ConsoleCalculator/Program.cs
+++ Calculator/ConsoleCalculator/Program.cs
@@ -2,33 +2,96 @@
 
 public class Calculator
 {
-    public static void Main()
+    static void Main(string[] args)
     {
-        int Number = 0;
-
-        while (Number == 0)
+        while (true)
         {
-            Console.Write("Enter number: ");
-            try
+            Console.Title = "Calculator Menu";
+            Console.WriteLine("Welcome, What Would you like to do");
+            Console.WriteLine("");
+            Console.WriteLine("[1]. Add Number");
+            Console.WriteLine("[2]. Subtract Number");
+            Console.WriteLine("[3]. Multiply Number");
+            Console.WriteLine("[4]. Divide Number");
+            Console.WriteLine("[5] Exit");
+            Console.WriteLine("");
+            Console.WriteLine("Please choose from 1-5");
+
+            string input = Console.ReadLine();
+
+            if (input == "1")
             {
-                Number = Convert.ToInt32(Console.ReadLine());
-                if (Number < 1 || Number > 10)
-                {
-                    Console.WriteLine("Please enter number between 1 to 10");
-                    Number = 0;
-                }
+                Add();
             }
-            catch (Exception)
+
+            else if (input == "2")
             {
-                Console.WriteLine("Please enter numeric value number between 1 to 10");
+                Multiply();
+            }
+            else if (input == "3")
+            {
+                Divide();
+            }
+
+            else if (input == "4")
+            {
+                Subtract();
+            }
+
+            else if (input == "5")
+            {
+                Console.WriteLine("Good Bye");
+                Environment.Exit(0);
             }
         }
+    }
 
-        for (int i = 0; i <= 10; i++)
-        {
-            if (i == 11)
-                break;
-            Console.WriteLine("{0} x {1} = {2}", Number, i , i * Number);
-        }
+    static decimal Add()
+    {
+        Console.WriteLine("Enter First Number");
+        var FN = Decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Nnumber");
+        var SN = Decimal.Parse(Console.ReadLine());
+
+        var Add = FN + SN;
+        Console.WriteLine($"Total = {Add}");
+        return (Add);
+    }
+    static decimal Multiply()
+    {
+        Console.WriteLine("Enter First Number");
+        var FN = Decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Nnumber");
+        var SN = Decimal.Parse(Console.ReadLine());
+
+        var Multiply = FN * SN;
+        Console.WriteLine($"Total = {Multiply}");
+        return (Multiply);
+    }
+    static decimal Divide()
+    {
+        Console.WriteLine("Enter First Number");
+        var FN = Decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Nnumber");
+        var SN = Decimal.Parse(Console.ReadLine());
+
+        var Divide = FN / SN;
+        Console.WriteLine($"Total = {Divide}");
+        return (Divide);
+    }
+    static decimal Subtract()
+    {
+        Console.WriteLine("Enter First Number");
+        var FN = Decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Nnumber");
+        var SN = Decimal.Parse(Console.ReadLine());
+
+        var Subtract = Convert.ToDecimal(FN - SN);
+        Console.WriteLine($"Total = {Subtract}");
+        return (Subtract);
     }
 }
