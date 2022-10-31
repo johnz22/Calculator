aea882dd6911b08318c4772bb7c4087d23985b26
John Moussa <John_z22@hotmail.com>
2022-11-01 02:48:56 +1100

    My First Age Calculator

---
+++ DateOfBirthCalc/DateOfBirthCalc/Program.cs
@@ -0,0 +1,17 @@
+﻿
+            Console.WriteLine("Hello, My First DOB Calc!");
+            Console.WriteLine();
+
+            var Today = DateTime.UtcNow;
+            Console.WriteLine("Type in your D.O.B in DD-MM-YYYY format:");
+
+            var MyBirthday = Console.ReadLine();
+
+            var myBirthdayDate = Convert.ToDateTime(MyBirthday);
+
+            var MyAge = Today.Year - myBirthdayDate.Year;
+
+            if (myBirthdayDate.Date > Today.AddYears(-MyAge)) MyAge--;
+
+            Console.WriteLine($"You are {MyAge} years old");
+            Console.ReadLine();
